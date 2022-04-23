37e9014781c2ff50331c54668f771ee728f3a735
daniel <Daniel Correa>
2022-04-23 16:12:08 -0500

    Add pez to totalPorTipo()

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -41,6 +41,7 @@ public class Animal {
 		return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" + 
 		"Aves: " + Ave.cantidadAves() + "\n" + 
 		"Reptiles: " + Reptil.cantidadReptiles() + "\n" + 
+		"Peces: " + Pez.cantidadPeces() + "\n" +  
 		"Anfibios: "+ Anfibio.cantidadAnfibios();
 	}
 	
